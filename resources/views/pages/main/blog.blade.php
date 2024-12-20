4f161c846f18a5d4d7febac5f9e5122ea0ab9852
Suh-Edmond <suhedmond25@yahoo.com>
2024-12-20 13:30:16 +0100

    reduce description length

--- resources/views/pages/main/blog.blade.php
+++ resources/views/pages/main/blog.blade.php
@@ -75,11 +75,10 @@
                                     <img src="{{asset($blog->getSingleBlogImage($blog->id))}}" alt="" width="100%">
                                     <div class="blog_details">
                                         <a href="{{route('show-blog', ['slug'=>$blog->slug])}}"><h2>{{$blog->title}}</h2></a>
-                                        <p>{!! substr($blog->description, 0, 300) !!}</p>
+                                        <p>{!! substr($blog->description, 0, 300) !!}</p><br><br><br>
+                                        <a href="{{route('show-blog', ['slug'=>$blog->slug])}}" class="white_bg_btn mt-4">View More</a>
                                     </div>
-                                    <div>
-{{--                                        <a href="{{route('show-blog', ['slug'=>$blog->slug])}}" class="white_bg_btn">View More</a>--}}
-                                    </div>
+
                                 </div>
                             </div>
                         </article>
