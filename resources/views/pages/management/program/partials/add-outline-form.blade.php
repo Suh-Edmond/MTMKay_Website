ac66fa198a34f0d259da9accd2303121fab42fbc
Suh-Edmond <suhedmond25@yahoo.com>
2024-12-20 01:40:18 +0100

    make topic to be a textarea field

--- resources/views/pages/management/program/partials/add-outline-form.blade.php
+++ resources/views/pages/management/program/partials/add-outline-form.blade.php
@@ -12,7 +12,8 @@
         </div>
         <div class="mt-4">
             <x-input-label for="topic" value="Topic" />
-            <x-text-input id="topic" name="topic" type="text" class="mt-1 block w-full"   :value="old('topic')" required   autocomplete="topic" />
+            <textarea id="topic" name="topic" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >{{old('topic',$program->topic ?? '')}}</textarea>
+
             <x-input-error class="mt-2" :messages="$errors->get('topic')" />
         </div>
 
