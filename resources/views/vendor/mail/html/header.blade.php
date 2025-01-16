f35175ba8a47bbf14526b3ed6db957cf2a0a22fb
Suh-Edmond <suhedmond25@yahoo.com>
2025-01-16 05:25:05 +0100

    change fetch query for blog to sent to subscribers

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'MTMKay')
-<img src="{{asset('img/company/mtmkay_logo.png')}}" class="logo" alt="MTMKay Logo">
+<img src="{{asset('img/company/mtmkay_logo.png')}}" class="logo" alt="MTMKay Logo" height="100px" width="100px">
 @else
 {{ $slot }}
 @endif
