923ad01325e49c29f0d235fc12aac8d9a2315c03
Suh-Edmond <suhedmond25@yahoo.com>
2025-01-16 13:01:20 +0100

    update delay in job execution

--- app/Console/Commands/SendBlogNotification.php
+++ app/Console/Commands/SendBlogNotification.php
@@ -26,6 +26,6 @@ class SendBlogNotification extends Command
      */
     public function handle()
     {
-        dispatch(new BlogNotificationsJob());
+        dispatch(new BlogNotificationsJob())->delay(now()->addMinutes(5));
     }
 }
