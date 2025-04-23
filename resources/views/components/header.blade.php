bfd54b6ecc59a035d73f9aaff7b9405931000580
Suh-Edmond <suhedmond25@yahoo.com>
2025-04-23 12:40:37 +0100

    fix navigation on smaller screens

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -19,13 +19,14 @@
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <a class="navbar-brand logo_h logo_navbar" href="{{route('home')}}"><img src="img/company/mtmkay_logo.png" alt="" width="20%" height="20%"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+                <button class="navbar-toggler menu_nav_bar" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse offset nav_menu_item_list" id="navbarSupportedContent">
                     <ul class="nav navbar-nav menu_nav ml-auto">
                         <li class="nav-item {{ request()->routeIs('home') ? 'active' : ''}}">
                             <a class="nav-link " href="{{route('home')}}">Home</a>
